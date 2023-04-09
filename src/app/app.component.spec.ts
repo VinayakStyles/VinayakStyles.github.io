f61e9ca7a5005580f04a204a3f943c36aee85b36
Dipender Sharma <dipendersharma989696@gmail.com>
2023-04-09 11:22:52 +0530

    Added demo Pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'VinayakStyles'`, () => {
+  it(`should have as title 'cloudkitchenweb'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('VinayakStyles');
+    expect(app.title).toEqual('cloudkitchenweb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('VinayakStyles app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cloudkitchenweb app is running!');
   });
 });
