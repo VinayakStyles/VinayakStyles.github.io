f8a622f56744175b6c1976e775880c0943de39f9
Dipender Sharma <dipendersharma989696@gmail.com>
2023-04-09 15:37:30 +0530

    Added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MenuComponent } from './pages/menu/menu.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { MenupageComponent } from './pages/menupage/menupage.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { MenupageComponent } from './pages/menupage/menupage.component';
     MenuComponent,
     AboutComponent,
     ContactComponent,
-    MenupageComponent
+    MenupageComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
